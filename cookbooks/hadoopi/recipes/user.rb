433f22b1328065be8bf8e8e7f3bd9175c8105af2
Andy Burgin <github@andyburgin.co.uk>
2016-07-05 21:30:56 +0000

    skip ssh key generation if already done

--- cookbooks/hadoopi/recipes/user.rb
+++ cookbooks/hadoopi/recipes/user.rb
@@ -22,7 +22,7 @@ end
 execute "generate ssh keys" do
 	user "hduser"
 	group "hadoop"
-	command "ssh-keygen -t rsa -f /home/hduser/.ssh/id_rsa -q -P \"\""
+	command "if [ ! -f /home/hduser/.ssh/id_rsa ]; then  ssh-keygen -t rsa -f /home/hduser/.ssh/id_rsa -q -P \"\"; fi"
 end
 
 execute "create authorized keys" do
