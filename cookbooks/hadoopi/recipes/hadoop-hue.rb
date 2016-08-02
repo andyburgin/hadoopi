cf3f1aa64fc18143a71ad08cc8802356b391f6c5
Andy Burgin <github@andyburgin.co.uk>
2016-08-02 19:15:45 +0000

    add hue install

---
+++ cookbooks/hadoopi/recipes/hadoop-hue.rb
@@ -0,0 +1,16 @@
+
+remote_file "/opt/hue-3.10.0.armf.tar.gz" do
+    source "http://192.168.2.30:8000/hue-3.10.0.armf.tar.gz"
+    action :create_if_missing
+end
+
+execute "unpack hue" do
+	command "tar -zxvf /opt/hue-3.10.0.armf.tar.gz -C /opt/"
+	user "root"
+end
+
+execute "create hue symlink" do
+	command "if [ ! -d /opt/hue ]; then ln -s /opt/hue-3.10.0 /opt/hue; fi"
+	user "root"
+end
+
