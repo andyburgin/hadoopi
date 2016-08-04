9f7d35d88fa78122e59be16785dc55cf7dce921e
Andy Burgin <github@andyburgin.co.uk>
2016-08-04 20:09:41 +0000

    unpack new hue binary and user

--- cookbooks/hadoopi/recipes/hadoop-hue.rb
+++ cookbooks/hadoopi/recipes/hadoop-hue.rb
@@ -1,3 +1,4 @@
+user "hue"
 
 remote_file "/opt/hue-3.10.0.armf.tar.gz" do
     source "http://192.168.0.9:8000/hue-3.10.0.armf.tar.gz"
@@ -5,12 +6,13 @@ remote_file "/opt/hue-3.10.0.armf.tar.gz" do
 end
 
 execute "unpack hue" do
-	command "tar -zxvf /opt/hue-3.10.0.armf.tar.gz -C /opt/"
-	user "root"
+        command "tar -zxvf /opt/hue-3.10.0.armf.tar.gz -C /opt/"
+        user "root"
 end
 
-execute "create hue symlink" do
-	command "if [ ! -d /opt/hue ]; then ln -s /opt/hue-3.10.0 /opt/hue; fi"
-	user "root"
+directory "/opt/hue" do
+        owner "hue"
+        group "hue"
+        mode  "0755"
 end
 
