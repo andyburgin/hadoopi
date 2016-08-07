ce082fd769b2e61306b3c5aaf819044ab6bf77a3
Andy Burgin <github@andyburgin.co.uk>
2016-08-07 19:08:20 +0000

    downgrade hive to 1.x

--- cookbooks/hadoopi/recipes/hadoop-hive.rb
+++ cookbooks/hadoopi/recipes/hadoop-hive.rb
@@ -1,17 +1,17 @@
 user "hue"
 
 remote_file "/opt/apache-hive-2.1.0-bin.tar.gz" do
-    source "http://www.mirrorservice.org/sites/ftp.apache.org/hive/stable-2/apache-hive-2.1.0-bin.tar.gz"
+    source "http://www.mirrorservice.org/sites/ftp.apache.org/hive/stable-2/apache-hive-1.2.0-bin.tar.gz"
     action :create_if_missing
 end
 
 execute "unpack hive" do
-        command "tar -zxvf /opt/apache-hive-2.1.0-bin.tar.gz -C /opt/"
+        command "tar -zxvf /opt/apache-hive-1.2.0-bin.tar.gz -C /opt/"
         user "root"
 end
 
 execute "create hive symlink" do
-        command "if [ ! -d /opt/hive ]; then ln -s /opt/apache-hive-2.1.0-bin /opt/hive; fi"
+        command "if [ ! -d /opt/hive ]; then ln -s /opt/apache-hive-1.2.0-bin /opt/hive; fi"
         user "root"
 end
 
@@ -21,7 +21,7 @@ execute "change hive file permissions" do
 end
 
 execute "change hive file permissions" do
-        command "chown -R hduser:hadoop /opt/apache-hive-2.1.0-bin"
+        command "chown -R hduser:hadoop /opt/apache-hive-1.2.0-bin"
         user "root"
 end
 
