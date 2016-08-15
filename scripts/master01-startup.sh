fb075cdf5406c40dd891313703731a0a0c5f2b3f
Andy Burgin <github@andyburgin.co.uk>
2016-08-15 18:55:30 +0000

    add startup scripts

---
+++ scripts/master01-startup.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Start dfs"
+su hduser - -c "/opt/hadoop/sbin/start-dfs.sh"
+
+echo "Start yarn"
+su hduser - -c "/opt/hadoop/sbin/start-yarn.sh"
+
+echo "Start hbase"
+su hduser - -c "/opt/hbase/bin/start-hbase.sh"
+
+echo "Start hbase thrift server"
+su hduser - -c "nohup /opt/hbase/bin/hbase thrift start > /opt/hbase/logs/hbase-thrift.out 2> /opt/hbase/logs/hbase-thrift.err &"
+
+#echo "Start zookeeper"
+#su hduser - -c "/opt/zookeeper/bin/zkServer.sh start"
+
+echo "Start hiveserver2"
+su hduser - -c "cd /opt/hive && nohup /opt/hive/bin/hive --service hiveserver2 > /opt/hadoop/logs/hive.out 2> /opt/hadoop/logs/hive.err &"
+
+echo "Start job history server"
+su hduser - -c "/opt/hadoop/sbin/mr-jobhistory-daemon.sh start historyserver"
+
+echo "Start hue"
+/opt/hue/build/env/bin/supervisor -d
+
+echo "Start oozie"
+su oozie - -c "cd /opt/oozie && bin/oozied.sh start"
---
+++ scripts/master01-stop.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "Stop dfs"
+su hduser - -c "/opt/hadoop/sbin/stop-dfs.sh"
+
+echo "Stop yarn"
+su hduser - -c "/opt/hadoop/sbin/stop-yarn.sh"
+
+echo "Stop hbase"
+su hduser - -c "/opt/hbase/bin/stop-hbase.sh"
+
+echo "Stop hbase thrift server"
+su hduser - -c "kill $(ps -ef | grep ThriftServer | awk '{print $2}')"
+
+#echo "Stop zookeeper"
+#su hduser - -c "/opt/zookeeper/bin/zkServer.sh stop"
+
+echo "Stop hiveserver2"
+su hduser - -c "kill $(ps -ef | grep HiveServer2 | awk '{print $2}')"
+
+echo "Stop job history server"
+su hduser - -c "/opt/hadoop/sbin/mr-jobhistory-daemon.sh stop historyserver"
+
+echo "Stop hue"
+killall supervisor
+
+echo "Stop oozie"
+su oozie - -c "cd /opt/oozie && bin/oozied.sh stop"
