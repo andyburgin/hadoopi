12a174d64933703743f5b05b217cc0346b192766
Andy Burgin <github@andyburgin.co.uk>
2016-08-28 20:09:31 +0000

    fix hbase log path

--- scripts/master01-startup.sh
+++ scripts/master01-startup.sh
@@ -10,7 +10,7 @@ echo "Start hbase"
 su hduser - -c "/opt/hbase/bin/start-hbase.sh"
 
 echo "Start hbase thrift server"
-su hduser - -c "nohup /opt/hbase/bin/hbase thrift start > /opt/hbase/logs/hbase-thrift.out 2> /opt/hbase/logs/hbase-thrift.err &"
+su hduser - -c "nohup /opt/hbase/bin/hbase thrift start > /opt/hadoop/logs/hbase-thrift.out 2> /opt/hadoop/logs/hbase-thrift.err &"
 
 #echo "Start zookeeper"
 #su hduser - -c "/opt/zookeeper/bin/zkServer.sh start"
