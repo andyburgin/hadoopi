7e1a6c47fdb101d318f3cf069aaeaea99cf51d4e
Andy Burgin <github@andyburgin.co.uk>
2016-10-10 21:50:35 +0000

    fix shutdown script for master02

--- scripts/master02-stop.sh
+++ scripts/master02-stop.sh
@@ -6,6 +6,6 @@ su hduser - -c "/opt/hbase/bin/stop-hbase.sh"
 echo "Stop hbase thrift server"
 su hduser - -c "kill $(ps -ef | grep ThriftServer | awk '{print $2}')"
 
-##echo "Stop Livy"
-##su hduser - -c "/opt/livy/bin/livy-server stop"
+echo "Stop Livy"
+su hduser - -c "/opt/livy/bin/livy-server stop"
 
