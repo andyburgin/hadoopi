daa06f2235f33413b0334ba9a78e6a35c0a3a4d6
Andy Burgin <github@andyburgin.co.uk>
2016-12-12 20:59:43 +0000

    fix typo

--- scripts/master02-startup.sh
+++ scripts/master02-startup.sh
@@ -9,6 +9,6 @@ su hduser - -c "nohup /opt/hbase/bin/hbase thrift start > /opt/hbase/logs/hbase-
 echo "Start Livy"
 su hduser - -c "export CLASSPATH=`hadoop classpath`; export SPARK_CONF_DIR=/opt/spark/conf; export SPARK_HOME=/opt/spark; export HUE_SECRET_KEY=hduser; /opt/livy/bin/livy-server start"
 
-exho "Start Solr"
+echo "Start Solr"
 su hduser - -c "/opt/solr/bin/solr start -m 256m -c -z master02:2181/solr -a '-Dsolr.hdfs.home=hdfs://master01:54310/solr'"
 
