270e3cffb219e7363c7a0455e28cc024c6f2b46c
Andy Burgin <github@andyburgin.co.uk>
2017-02-19 15:42:11 +0000

    fix spark download address

--- cookbooks/hadoopi/recipes/hadoop-spark-hdfs.rb
+++ cookbooks/hadoopi/recipes/hadoop-spark-hdfs.rb
@@ -1,7 +1,7 @@
 # unpack spark and copy libs to hdfs
 
 remote_file "/opt/spark-#{node['spark']['version']}-bin-hadoop#{node['spark']['hadoop-version']}.tgz" do
-    source ""https://archive.apache.org/dist/spark/spark-#{node['spark']['version']}/spark-#{node['spark']['version']}-bin-hadoop#{node['spark']['hadoop-version']}.tgz"
+    source "https://archive.apache.org/dist/spark/spark-#{node['spark']['version']}/spark-#{node['spark']['version']}-bin-hadoop#{node['spark']['hadoop-version']}.tgz"
     action :create_if_missing
 end
 
