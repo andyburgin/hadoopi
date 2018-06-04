00e107945b53612afa4b09f282c96410bc8f6f2f
name <none@none.home>
2018-06-04 21:28:57 +0100

    Add original binary files from hadoop for jmx monitoring

---
+++ cookbooks/hadoopi/recipes/hadoop-prometheus-jmx.rb
@@ -0,0 +1,36 @@
+template "/opt/hadoop/bin/hdfs" do
+        source "hdfs.erb"
+        mode 0755
+        user 'hduser'
+        group 'hadoop'
+end
+
+template "/opt/hadoop/bin/yarn" do
+        source "yarn.erb"
+        mode 0755
+        user 'hduser'
+        group 'hadoop'
+end
+
+template "/opt/hadoop/etc/hadoop/hadoop-env.sh" do
+        source "hadoop-env.sh.erb"
+        mode 0644
+        user 'hduser'
+        group 'hadoop'
+end
+
+template "/opt/hadoop/sbin/mr-jobhistory-daemon.sh" do
+        source "mr-jobhistory-daemon.sh.erb"
+        mode 0755
+        user 'hduser'
+        group 'hadoop'
+end
+
+template "/opt/hbase/bin/hbase" do
+        source "hbase.erb"
+        mode 0755
+        user 'hduser'
+        group 'hadoop'
+        only_if { ::File.exist?('/opt/hbase') }
+end
+
