9f3199af2028e4f9ffc67a5e238473d7b3bc0965
name <none@none.home>
2018-06-06 22:15:45 +0100

    Add grafana datasource and dashboards

--- cookbooks/hadoopi/recipes/grafana.rb
+++ cookbooks/hadoopi/recipes/grafana.rb
@@ -13,6 +13,59 @@ execute "install package" do
         user "root"
 end
 
+# grafana datasource
+template "/etc/grafana/provisioning/datasources/prometheus.yaml" do
+        source "prometheus.yaml.erb"
+        mode 0644
+end
+
+# grafana dashboards
+template "/etc/grafana/provisioning/dashboards/hadoopi.yaml" do
+        source "hadoopi.yaml.erb"
+        mode 0644
+end
+
+template "/etc/grafana/provisioning/dashboards/Hadoop.json" do
+        source "Hadoop.json.erb"
+        mode 0644
+end
+
+template "/etc/grafana/provisioning/dashboards/HBase.json" do
+        source "HBase.json.erb"
+        mode 0644
+end
+
+template "/etc/grafana/provisioning/dashboards/JVM.json" do
+        source "JVM.json.erb"
+        mode 0644
+end
+
+template "/etc/grafana/provisioning/dashboards/MySQL.json" do
+        source "MySQL.json.erb"
+        mode 0644
+end
+
+template "/etc/grafana/provisioning/dashboards/Named_processes.json" do
+        source "Named_processes.json.erb"
+        mode 0644
+end
+
+template "/etc/grafana/provisioning/dashboards/Node_Exporter_Full.json" do
+        source "Node_Exporter_Full.json.erb"
+        mode 0644
+end
+
+template "/etc/grafana/provisioning/dashboards/Node_Exporter_Server_Metrics.json" do
+        source "Node_Exporter_Server_Metrics.json.erb"
+        mode 0644
+end
+
+template "/etc/grafana/provisioning/dashboards/Zookeeper.json" do
+        source "Zookeeper.json.erb"
+        mode 0644
+end
+
+# enable and start grafana
 execute "enable and start grafana" do
         command "systemctl enable grafana-server && systemctl restart grafana-server"
         user "root"
