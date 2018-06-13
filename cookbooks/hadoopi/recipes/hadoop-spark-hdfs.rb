d6ce76728ef77d137ef1d7c8d8a91127fc644491
name <none@none.home>
2018-06-13 22:35:44 +0100

    Add spark config

--- cookbooks/hadoopi/recipes/hadoop-spark-hdfs.rb
+++ cookbooks/hadoopi/recipes/hadoop-spark-hdfs.rb
@@ -10,10 +10,39 @@ execute "unpack spark" do
         user "root"
 end
 
+execute "create spark symlink" do
+        command "if [ ! -d /opt/spark ]; then ln -s /opt/spark-#{node['spark']['version']}-bin-hadoop#{node['spark']['hadoop-version']} /opt/spark; fi"
+        user "root"
+end
+
 execute "change spark file permissions" do
         command "chown -R hduser:hadoop /opt/spark-#{node['spark']['version']}-bin-hadoop#{node['spark']['hadoop-version']}"
         user "root"
 end
+
+execute "change spark symlink file permissions" do
+        command "chown -R hduser:hadoop /opt/spark"
+        user "root"
+end
+
+template "/opt/spark/conf/spark-env.sh" do
+        source "spark-env.sh.erb"
+        mode 0644
+        user 'hduser'
+        group 'hadoop'
+end
+
+template "/opt/spark/conf/spark-defaults.conf" do
+        source "spark-defaults.conf.erb"
+        mode 0644
+        user 'hduser'
+        group 'hadoop'
+end
+
+
+
+
+
 
 # copy spark lib files to hdfs
 execute "start hdfs for copying of spark libs" do
